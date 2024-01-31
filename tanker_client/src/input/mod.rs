806e0073fbd4b847fc957dcfae99cbc70629f522
fastjonh <fastjonh@gmail.com>
2024-01-31 01:25:08 -0300

    adicionando zoom no scroll

--- tanker_client/src/input/mod.rs
+++ tanker_client/src/input/mod.rs
@@ -5,7 +5,8 @@ pub struct InputPlugin;
 
 impl Plugin for InputPlugin {
     fn build(&self, app: &mut bevy::prelude::App) {
-        app.add_systems(Update, Self::input_system);
+        app.add_systems(Update, Self::input_system)
+            .add_systems(Update, Self::camera_zoom_system);
     }
 }
 
@@ -24,5 +25,16 @@ impl InputPlugin {
         if direction != Vec3::ZERO {
             move_events.send(MoveDirection(direction.normalize_or_zero()));
         }
+    }
+
+    fn camera_zoom_system(
+        mut scroll_evts: EventReader<MouseWheel>,
+        mut query: Query<&mut Transform, With<Camera>>,
+    ) {
+        let mut transform = query.single_mut();
+
+        let scroll: f32 = scroll_evts.read().map(|e| e.y).sum();
+
+        transform.translation.z += scroll;
     }
 }
